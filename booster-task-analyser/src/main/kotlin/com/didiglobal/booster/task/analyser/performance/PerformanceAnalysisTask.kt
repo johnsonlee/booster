9c25a4cb8bb90ca0bfad270f6e6d884a85886955
johnsonlee <g.johnsonlee@gmail.com>
2022-06-11 12:53:12 +0800

    Refactoring `booster-task-analyser` to support reporting API for performance analysis

--- booster-task-analyser/src/main/kotlin/com/didiglobal/booster/task/analyser/performance/PerformanceAnalysisTask.kt
+++ booster-task-analyser/src/main/kotlin/com/didiglobal/booster/task/analyser/performance/PerformanceAnalysisTask.kt
@@ -4,11 +4,19 @@ import com.android.build.gradle.api.BaseVariant
 import com.didiglobal.booster.gradle.extension
 import com.didiglobal.booster.kotlinx.file
 import com.didiglobal.booster.task.analyser.Build
+import com.didiglobal.booster.task.analyser.performance.reporting.PerformanceReports
+import com.didiglobal.booster.task.analyser.performance.reporting.PerformanceReportsImpl
+import com.didiglobal.booster.task.analyser.reference.reporting.ReferenceReports
+import com.didiglobal.booster.task.analyser.reference.reporting.ReferenceReportsImpl
 import com.didiglobal.booster.transform.artifacts
+import groovy.lang.Closure
+import org.gradle.api.Action
 import org.gradle.api.DefaultTask
+import org.gradle.api.reporting.Reporting
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
+import org.gradle.util.ClosureBackedAction
 import java.io.File
 
 /**
@@ -16,7 +24,7 @@ import java.io.File
  *
  * @author johnsonlee
  */
-open class PerformanceAnalysisTask : DefaultTask() {
+open class PerformanceAnalysisTask : DefaultTask(), Reporting<PerformanceReports> {
 
     @get:Internal
     lateinit var variant: BaseVariant
@@ -28,8 +36,38 @@ open class PerformanceAnalysisTask : DefaultTask() {
     val variantName: String
         get() = variant.name
 
+    @get:Internal
+    val _reports: PerformanceReports by lazy {
+        project.objects.newInstance(PerformanceReportsImpl::class.java, this)
+    }
+
+    override fun getReports(): PerformanceReports = _reports
+
+    override fun reports(closure: Closure<*>): PerformanceReports {
+        return reports(ClosureBackedAction(closure))
+    }
+
+    override fun reports(configureAction: Action<in PerformanceReports>): PerformanceReports {
+        configureAction.execute(_reports)
+        return _reports
+    }
+
     @TaskAction
     fun analyse() {
+        if ((!reports.html.isEnabled) && (!reports.dot.isEnabled) && (!reports.json.isEnabled)) {
+            logger.warn("""
+                Please enable reference analysis reports with following configuration:
+                
+                tasks.withType(${javaClass.simpleName}) {
+                    reports {
+                        html.enabled = true
+                        json.enabled = true
+                        dot.enabled = true
+                    }
+                }
+            """.trimIndent())
+        }
+
         val classpath = supplier().let {
             if (it.isDirectory) {
                 it.listFiles()?.toList() ?: emptyList()
