818f02662643e5b6bec44d6738df66f8b790072c
johnsonlee <g.johnsonlee@gmail.com>
2022-06-13 13:36:16 +0800

    Fix issue #319

--- booster-task-list-shared-library/src/main/kotlin/com/didiglobal/booster/task/so/ListSharedLibraryVariantProcessor.kt
+++ booster-task-list-shared-library/src/main/kotlin/com/didiglobal/booster/task/so/ListSharedLibraryVariantProcessor.kt
@@ -1,12 +1,21 @@
 package com.didiglobal.booster.task.so
 
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.api.BaseVariant
 import com.android.build.gradle.internal.tasks.factory.dependsOn
 import com.didiglobal.booster.BOOSTER
+import com.didiglobal.booster.gradle.dependencies
+import com.didiglobal.booster.gradle.getAndroid
+import com.didiglobal.booster.gradle.getTaskName
+import com.didiglobal.booster.gradle.isAndroid
 import com.didiglobal.booster.gradle.project
 import com.didiglobal.booster.task.spi.VariantProcessor
 import com.google.auto.service.AutoService
+import org.gradle.api.Project
 import org.gradle.api.UnknownTaskException
+import org.gradle.api.artifacts.component.ProjectComponentIdentifier
 
 private const val TASK_NAME = "listSharedLibraries"
 
@@ -14,7 +23,7 @@ private const val TASK_NAME = "listSharedLibraries"
 class ListSharedLibraryVariantProcessor : VariantProcessor {
 
     override fun process(variant: BaseVariant) {
-        variant.project.tasks.let { tasks ->
+        val listSharedLibrary = variant.project.tasks.let { tasks ->
             val listSharedLibraries = try {
                 tasks.named(TASK_NAME)
             } catch (e: UnknownTaskException) {
@@ -28,8 +37,21 @@ class ListSharedLibraryVariantProcessor : VariantProcessor {
                 it.description = "List the shared libraries that current project depends on for ${variant.name}"
                 it.outputs.upToDateWhen { false }
                 it.variant = variant
-            }.also {
-                listSharedLibraries.dependsOn(it)
+            }
+        }
+
+        variant.project.afterEvaluate { project ->
+            variant.dependencies.asSequence().map {
+                it.id.componentIdentifier
+            }.filterIsInstance<ProjectComponentIdentifier>().mapNotNull {
+                project.rootProject.findProject(it.projectPath)
+            }.filter(Project::isAndroid).map {
+                when (val android = it.getAndroid<BaseExtension>()) {
+                    is LibraryExtension -> android.libraryVariants
+                    else -> emptyList<BaseVariant>()
+                }
+            }.flatten().toList().forEach {
+                listSharedLibrary.dependsOn(it.getTaskName("createFullJar"))
             }
         }
     }
