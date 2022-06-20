7e3991808126297db513e0371cc040dcb7528fc5
johnsonlee <g.johnsonlee@gmail.com>
2022-06-20 23:59:03 +0800

    Add `booster-cha-asm` to support ASM based CHA

---
+++ booster-cha-asm/src/test/kotlin/com/didiglobal/booster/cha/ClassHierarchyTest.kt
@@ -0,0 +1,42 @@
+package com.didiglobal.booster.cha
+
+import com.didiglobal.booster.build.AndroidSdk
+import com.didiglobal.booster.cha.asm.AsmClassSet
+import com.didiglobal.booster.cha.asm.from
+import java.lang.management.ManagementFactory
+import java.time.Duration
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+class ClassHierarchyTest {
+
+    private val threadMxBean = ManagementFactory.getThreadMXBean()
+
+    @Test
+    fun `lookup children class of Context`() {
+        val androidSdk =  listOf(ClassSet.of(emptyList()), ClassSet.from(AndroidSdk.getAndroidJar())).fold()
+
+        // build class hierarchy
+        val t1 = threadMxBean.currentThreadCpuTime
+        val hierarchy = ClassHierarchy(androidSdk)
+        println("t1: ${Duration.ofNanos(threadMxBean.currentThreadCpuTime - t1)}")
+
+        // acquire subtypes of Context
+        val t2 = threadMxBean.currentThreadCpuTime
+        val childrenOfContext = hierarchy.getDerivedTypes("android/content/Context")
+        println("t2 : ${Duration.ofNanos(threadMxBean.currentThreadCpuTime - t2)}")
+        assertNotNull(childrenOfContext)
+        assertTrue("Children of android/content/Context not found") {
+            childrenOfContext.isNotEmpty()
+        }
+        println(childrenOfContext.joinToString(", ") { it.name })
+
+        // acquire subtypes of Context
+        val t3 = threadMxBean.currentThreadCpuTime
+        val childrenOfThread = hierarchy.getDerivedTypes("java/lang/Thread")
+        println("t3 : ${Duration.ofNanos(threadMxBean.currentThreadCpuTime - t3)}")
+        println(childrenOfThread.joinToString(", ") { it.name })
+    }
+
+}
