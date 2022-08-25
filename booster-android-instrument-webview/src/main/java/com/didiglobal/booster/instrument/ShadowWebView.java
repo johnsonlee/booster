e276506675a284e28de5647f570adf3cff76bcf2
johnsonlee <g.johnsonlee@gmail.com>
2022-08-26 00:43:16 +0800

    Fix issue #294 by guessing the method `startYourEngines`

--- booster-android-instrument-webview/src/main/java/com/didiglobal/booster/instrument/ShadowWebView.java
+++ booster-android-instrument-webview/src/main/java/com/didiglobal/booster/instrument/ShadowWebView.java
@@ -10,8 +10,13 @@ import android.os.SystemClock;
 import android.util.Log;
 import android.webkit.WebView;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import static com.didiglobal.booster.instrument.Constants.TAG;
-import static com.didiglobal.booster.instrument.Reflection.invokeMethod;
 import static com.didiglobal.booster.instrument.Reflection.invokeStaticMethod;
 
 /**
@@ -42,8 +47,37 @@ public class ShadowWebView {
         try {
             final long t0 = SystemClock.uptimeMillis();
             final Object provider = invokeStaticMethod(Class.forName("android.webkit.WebViewFactory"), "getProvider");
-            invokeMethod(provider, "startYourEngines", new Class[]{boolean.class}, new Object[]{true});
-            Log.i(TAG, "Start chromium engine complete: " + (SystemClock.uptimeMillis() - t0) + " ms");
+
+            try {
+                final Method startYourEngines = provider.getClass().getDeclaredMethod("startYourEngines", boolean.class);
+                startYourEngines.setAccessible(true);
+                startYourEngines.invoke(provider, true);
+                Log.i(TAG, "Start chromium engine complete: " + (SystemClock.uptimeMillis() - t0) + " ms");
+            } catch (final Throwable t) {
+                final Set<Method> candidates = new HashSet<>();
+                final Method[] methods = provider.getClass().getSuperclass().getDeclaredMethods();
+
+                for (final Method m : methods) {
+                    if (0 == (Modifier.STATIC & m.getModifiers())
+                            && 1 == m.getParameterCount()
+                            && void.class.equals(m.getReturnType())
+                            && boolean.class.equals(m.getParameterTypes()[0])) {
+                        candidates.add(m);
+                    }
+                }
+
+                if (candidates.size() == 1) {
+                    final Method startYourEngines = candidates.iterator().next();
+                    startYourEngines.setAccessible(true);
+                    startYourEngines.invoke(provider, true);
+                    Log.i(TAG, "Start chromium engine from " + provider.getClass().getName() + "." + startYourEngines.getName() + "(boolean) complete: " + (SystemClock.uptimeMillis() - t0) + " ms");
+                } else if (candidates.size() > 1) {
+                    Log.w(TAG, "Method " + provider.getClass().getName() + ".startYourEngines(boolean) not found, multiple candidates found: " + Arrays.toString(candidates.toArray()));
+                } else {
+                    Log.w(TAG, "Method " + provider.getClass().getName() + ".startYourEngines(boolean) not found", t);
+                }
+            }
+
             if (Build.VERSION.SDK_INT >= 28) {
                 String processName = Application.getProcessName();
                 String packageName = context.getPackageName();
